1f67e94bbf42ddaf2d9ec0ac0d78187b1d08cb77
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-05-19 18:16:37 -0300

    resultado

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
 import CalcForm from './components/CalcForm.vue';
+import Resultado from './components/Resultado.vue';
 </script>
 
 <template>
   <Header />
   <CalcForm />
+  <Resultado />
 </template>
 
 <style scoped></style>
---
+++ src/components/Resultado.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div class="container text-center">
+    <h3>Resultado</h3>
+    <span class="d-inline-block p-5 bg-warning h1 rounded">50</span>
+  </div>
+</template>

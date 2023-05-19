728e96f036ff2adf0283e303734270925feafd38
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-05-19 18:07:40 -0300

    formulario

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue';
+import CalcForm from './components/CalcForm.vue';
 </script>
 
 <template>
   <Header />
+  <CalcForm />
 </template>
 
 <style scoped></style>
---
+++ src/components/CalcForm.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <form class="container py-5">
+    <div class="row flex justify-content-center">
+      <div class="col-md-4">
+        <input
+          type="number"
+          class="form-control mb-3"
+          placeholder="Digite um número"
+        />
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Digite um número"
+        />
+      </div>
+      <div class="col-auto">
+        <select class="form-select">
+          <option selected>Selecione uma opção</option>
+          <option value="somar">Somar</option>
+          <option value="subtrair">Subtrair</option>
+          <option value="multiplicar">Multiplicar</option>
+          <option value="dividir">Dividir</option>
+        </select>
+      </div>
+    </div>
+  </form>
+</template>
